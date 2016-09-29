e86ec2bde7013c0b4de121a886b61f112a1bbbbd
kopmba <slhenny30@gmail.com>
2016-09-30 01:34:10 +0200

    Update heroku-web.js

--- heroku-web.js
+++ heroku-web.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use('/api', jsonServer.router('db.json'));
+app.use('/api', jsonServer.router('db.json'));
 app.use(express.static(path.join(__dirname, '')));
 
 
@@ -20,5 +20,5 @@ app.get('*', function(req, res) {
 });
 
 app.listen(port, function() {
-  console.log('Node app is running on port', port);
+  console.log('Node app is running on port ', port);
 });
